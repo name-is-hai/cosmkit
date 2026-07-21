9a8a3d1e9c79875d0b2e697c9ff4bcb6a521d2aa
name-is-hai <90458368+name-is-hai@users.noreply.github.com>
2026-07-21 15:54:44 +0700

    fix(zsh): comment zsh completion

--- config/zsh/zshrc.d/60-completion.zsh
+++ config/zsh/zshrc.d/60-completion.zsh
@@ -1,10 +1,10 @@
 autoload -Uz compinit
 
-zstyle ':completion:*' menu select
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-zstyle ':completion:*' format '%F{yellow}-- %d --%f'
-
-zstyle ':completion:*' use-cache on
+# zstyle ':completion:*' menu select
+# zstyle ':completion:*' group-name ''
+# zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+# zstyle ':completion:*' format '%F{yellow}-- %d --%f'
+#
+# zstyle ':completion:*' use-cache on
 
 compinit -C
